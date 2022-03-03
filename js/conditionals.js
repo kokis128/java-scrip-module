2ec42869585d82da390e965e8e2f4cc20b6d54a1
kokis128 <jorgevalentini76@gmail.com>
2022-03-03 08:11:28 -0300

    Conditional if else

---
+++ js/conditionals.js
@@ -0,0 +1,27 @@
+let numero1=6;
+let numero2=25;
+let numero3=6;
+let active=true; //con el signo ! invierto su valor
+
+let resultado = (numero1!=numero2);
+
+console.log(resultado);
+
+
+let nombre1 ="jose";
+let nombre2="malcom";
+let nombre3="Jose";
+
+let resultado2 = (numero1==numero2);
+console.log(resultado2);
+
+
+let canAccess = prompt("ingrese su edad");
+console.log("Edad ingesada por el usuario: ",canAccess);
+
+if (canAccess >= 18) {
+    document.write("el usuario puede ingresar")
+
+}else{
+    document.write("El Usuario no puede ingresar")
+}
---
+++ js/operators.js
@@ -0,0 +1,23 @@
+let numero1=6;
+let numero2=25;
+let active=true; //con el signo ! invierto su valor
+let suma=numero1+numero2;
+let resta=numero1-numero2;
+let division=numero1/numero2;
+let multiplicacion=numero1*numero2;
+let modulo= numero1 % 3;
+let numeroSiguiente=++numero1;
+let numeroAnterior=--numero2;
+let active=true; //con el signo ! invierto su valor
+console.log('Numero1 = '+ numero1);
+console.log('Numero2 = '+ numero2);
+console.log("-----------------------");
+console.log('resta = '+ resta);
+console.log('division = '+ division);
+console.log('multiplicacion = '+ multiplicacion);
+console.log('modulo = '+ modulo);
+console.log('numeroSiguiente = '+ numeroSiguiente);
+console.log('numeroAnterior = '+ numeroAnterior);
+console.log('Activo = '+ !active);
+
+
